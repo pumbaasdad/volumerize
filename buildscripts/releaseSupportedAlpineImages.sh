4db3a9018e742da1c7e3ebb394a5117aa2466d7a
Felix Haase <felix.haase@feki.de>
2020-09-26 23:39:44 +0200

    fix issue with images not being pushed
    forgot to change release usage
    forgot changing echo to exec

--- buildscripts/releaseSupportedAlpineImages.sh
+++ buildscripts/releaseSupportedAlpineImages.sh
@@ -6,28 +6,27 @@ function pushImage() {
   local tagname=$1
   local repository=$2
 
-  echo  docker push fekide/volumerize:$tagname
+  exec docker push fekide/volumerize:$tagname
 }
 
 # Setting environment variables
 readonly CUR_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 readonly PUSH_REPOSITORY=$1
-
-printf '%b\n' ":: Release default image...."
 source $CUR_DIR/release.sh
 
+printf '%b\n' ":: Release default image...."
+release
 pushImage $IMAGE_TAG $PUSH_REPOSITORY
 
 
 export IMAGE_TYPE=mongodb
 printf '%b\n' ":: Release ${IMAGE_TYPE} image...."
-source $CUR_DIR/release.sh
-
+release
 pushImage $IMAGE_TAG $PUSH_REPOSITORY
 
 
 export IMAGE_TYPE=mysql
 printf '%b\n' ":: Release ${IMAGE_TYPE} image...."
-source $CUR_DIR/release.sh
+release
 
 pushImage $IMAGE_TAG $PUSH_REPOSITORY
