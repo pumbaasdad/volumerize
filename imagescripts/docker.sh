6c4f5df4315731bae90a81e31f35a1075686fd3e
Felix Haase <felix.haase@feki.de>
2020-09-27 22:07:04 +0200

    Get full docker task name instead of short id

--- imagescripts/docker.sh
+++ imagescripts/docker.sh
@@ -2,11 +2,43 @@
 
 set -o errexit
 
+# From  https://stackoverflow.com/a/42955871/6475604
+function parse_node() {
+  read title
+  id_start=0
+  name_start=`strindex "$title" NAME`
+  image_start=`strindex "$title" IMAGE`
+  node_start=`strindex "$title" NODE`
+  dstate_start=`strindex "$title" DESIRED`
+  id_length=name_start
+  name_length=`expr $image_start - $name_start`
+  node_length=`expr $dstate_start - $node_start`
+
+  read line
+  id=${line:$id_start:$id_length}
+  name=${line:$name_start:$name_length}
+  name=$(echo $name)
+  node=${line:$node_start:$node_length}
+  echo $name.$id
+  echo $node
+}
+
+strindex() { 
+  x="${1%%$2*}"
+  [[ "$x" = "$1" ]] && echo -1 || echo "${#x}"
+}
+
 function dockerTaskOfService() {
-  local service=$1
-  local tasksArray=
-  IFS=' ' read -r -a tasksArray <<< $(docker service ps --format "{{.ID}}" $service)
-  TASK_ID=${tasksArray[0]}
+  local exec_task=$1
+  local exec_instance=$2
+
+  if true; then 
+    read fn 
+    local docker_fullname=$fn
+    read nn
+    local docker_node=$nn 
+  fi < <( docker service ps -f name=$exec_task.$exec_instance --no-trunc -f desired-state=running $exec_task | parse_node )
+  SWARM_TASK=${docker_fullname}
 }
 
 function stopContainers() {
