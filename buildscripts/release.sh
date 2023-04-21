ca664d95d3957701d7ec0c4a1902a21997f7bd16
Felix Haase <felix.haase@feki.de>
2023-04-21 15:34:27 +0200

    slugify the branch or tag

--- buildscripts/release.sh
+++ buildscripts/release.sh
@@ -3,12 +3,17 @@
 #------------------
 # CONTAINER VARIABLES
 #------------------
+
+slugify () {
+  echo "$1" | iconv -t ascii//TRANSLIT | sed -r s/[~\^]+//g | sed -r s/[^a-zA-Z0-9\.]+/-/g | sed -r s/^-+\|-+$//g | tr A-Z a-z
+}
+
 function release() {
   local BUILD_VERSION=${BUILD_BRANCH:-${GITHUB_REF_NAME:-$(git branch | grep -e "^*" | cut -d' ' -f 2)}}
   if [ $BUILD_VERSION == master ]; then
     BUILD_VERSION=latest
   fi
-  local IMAGE_VERSION=${IMAGE_VERSION:-$BUILD_VERSION}
+  local IMAGE_VERSION=$(slugify ${IMAGE_VERSION:-$BUILD_VERSION})
 
   if [ -z $IMAGE_TYPE ]; then
     export IMAGE_TAG=${IMAGE_VERSION}
